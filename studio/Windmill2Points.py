9ef039f7c6e30f9b45feb0bc8bcdc33e5f986e12
ricardo <nocive.science@gmail.com>
2024-07-08 19:25:00 -0400

    no cambia la posicion a pesar de redefinir las condicionales

--- studio/Windmill2Points.py
+++ studio/Windmill2Points.py
@@ -16,15 +16,24 @@ class Windmill2Points(Scene):
         self.current_pivot = points[0]
 
         self.time = 0
+        self.diff_angle_0= np.abs(angle_of_vector(points[0])-line.get_angle())
+        self.diff_angle_1= np.abs(angle_of_vector(points[1])-line.get_angle())
+        
+        print('diff_angle_0: ', self.diff_angle_0)
+        print('diff_angle_1: ', self.diff_angle_1)
         
         def update_line(line, dt):
             # Rotar la línea alrededor del pivote actual
             self.time += dt
             print(self.time*10)
-            if (self.time)*10 % 2 == 0:
-                self.current_pivot=points[1]
-            elif (self.time)*10 % 2 == 1:
+            if self.diff_angle_0 < 0.1:
                 self.current_pivot=points[0]
+            elif self.diff_angle_1 < 0.1:
+                self.current_pivot=points[1]
+            # if (self.time)*10 % 2 == 0:
+            #     self.current_pivot=points[1]
+            # elif (self.time)*10 % 2 == 1:
+            #     self.current_pivot=points[0]
             line.rotate(dt * PI / 2, about_point=self.current_pivot)
             line.move_to(self.current_pivot)
 
